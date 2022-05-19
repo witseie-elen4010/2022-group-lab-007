88c13d4e4118558100158852fe2f6ca2a8491432
TristandeGroot <tristankyledegroot@gmail.com>
2022-05-19 22:54:26 +0200

    Login Page Frontend

---
+++ login.js
@@ -0,0 +1,9 @@
+function checkLogin () {
+  const username = document.getElementById('username').value
+  const password = document.getElementById('password').value
+  if (username === 'admin@example.com' && password === 'admin') {
+    alert('You have succesfully logged in!')
+  } else {
+    alert('Incorrect username/password!')
+  }
+}
