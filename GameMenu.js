f33853f375cafd781578e121ca051fda6726e178
ZunaidValodia <1624885@students.wits.ac.za>
2022-05-21 22:59:36 +0200

    Basic menu html and js setup

---
+++ GameMenu.js
@@ -0,0 +1,26 @@
+
+const Singlebutton = document.getElementById('SinglePlayer')
+const Multibutton = document.getElementById('Multiplayer')
+const Scorebutton = document.getElementById('Scoreboard')
+const Logoutbutton = document.getElementById('Logout')
+const Instbutton= document.getElementById('Instructions')
+
+Singlebutton.addEventListener('click', function () {
+    
+}, false)
+
+Multibutton.addEventListener('click', function () {
+
+}, false)
+
+Scorebutton.addEventListener('click', function () {
+
+}, false)
+
+Logoutbutton.addEventListener('click', function () {
+
+}, false)
+
+Instbutton.addEventListener('click', function () {
+    window.location.href = ("GameInstructions.html");
+}, false)
