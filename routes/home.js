4ff587d16728a3b3134d5dc5d6f5be75f05ac171
TristandeGroot <tristankyledegroot@gmail.com>
2022-05-26 23:23:31 +0200

    Prevent access to Game menu page unless logged in

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const session = require('express-session');
 
+// check if logged in
 router.get('/', (req, res) => {
-    res.render('users/gameMenu')
+    // if no token go to login page
+    if (!req.session.ID) {
+        //res.redirect('/users')
+        res.render('users/login')
+    // if there is a token continue to Game menu
+    } else {
+        res.render('users/gameMenu')
+    }
 })
 
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -10,21 +10,27 @@ const bcrypt = require('bcrypt')
 // create instance of express
 const router = express.Router()
 // import Express session for token creation
-const expSession = require('express-session');
-//const FileStore = require('session-file-store')(expSession)
+const session = require('express-session');
+const FileStore = require('session-file-store')(session)
 
 // ///////////////////////////////////////////////////
 // routes
 // ///////////////////////////////////////////////////
-router.get('/', (req, res) => {
-    console.log(req.query.username)
-    res.send('Login unsuccessful')
-})
 
 // rerouting to the login page should login fail
 router.get('/login', (req, res) => {
     res.render("users/login")
-})   
+}) 
+
+// use this function to destroy token from login
+router.get('/', (req, res) => {
+    req.session.destroy(function(err) {
+        if (err) {
+            console.error(err);
+        } 
+    });
+    res.render('users/login')
+})
 
 // ///////////////////////////////////////////////////
 // validate login by comparing users from server
@@ -35,32 +41,40 @@ router.post('/', (req, res) => {
     let username_ = req.body.username
     let password_ = req.body.password
 
-  // Check the server database for the inputted username
+    // if no input into the username field 
+    if (username_.length === 0){
+        return res.redirect('/users/login')
+    }
+
+    // Check the server database for the inputted username
     database.pools
     .then((pool) => {
-      return pool.request()
-      // input to query is the username
-      .input('username', username_)
-      // SQL Query to server database table
-      .query('SELECT password FROM dbo.AccountTbl WHERE username = @username;')
-  })
+        return pool.request()
+        // input to query is the username
+        .input('username', username_)
+        // SQL Query to server database table
+        .query('SELECT userID, password FROM dbo.AccountTable WHERE username = @username;')
+    })
 
-  // Compare the input password and the server password 
-  .then(async (result) => {
+    // Compare the input password and the server password 
+    .then(async (result) => {
     // server password is hashed therefore bcrypt.compare is used
     if (await bcrypt.compare(password_, result.recordset[0].password)) {      
         // create a token for user if login succeeds
-      
+        sessData = req.session
+        sessData.ID = result.recordset[0].userID
+        console.log(req.session.ID)
         // if Login succeeds go to Game menu
         return res.redirect('/home')
     } else {
         // if Login fails remain on Login page
         return res.redirect('/users/login')
     }
-  })
-// Error with the server database
+
+    })
+    // Error with the server database
     .catch(err => {
-    res.send({ Error: err })
+        res.send({ Error: err })
     })
 
 })
