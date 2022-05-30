e301f747bdcbbe13c3a83912780d56b231dc1f6d
Momohaffejee <moreas816@gmail.com>
2022-05-30 02:58:30 +0200

    first testing commit

--- routes/SinglePlayer.js
+++ routes/SinglePlayer.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const app = express()
 
 router.get('/', (req, res) => {
     res.render('users/gamePage.ejs')
+    
 })
 
 module.exports = router
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello World!");
+  });
+  
+  module.exports = app
