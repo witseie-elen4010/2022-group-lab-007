052fd79aabcb754e2169c45fbf84f4ae67f3fe6f
TristandeGroot <tristankyledegroot@gmail.com>
2022-06-05 21:00:43 +0200

    Testing for Login system and account security

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -17,8 +17,8 @@ describe('Test routing, security and login:', () => {
         await page.waitForTimeout(1000);
         await browser.close();
      })
-     /*
-     it("Register button redirects to Create Account Page:", async () => {
+     
+     it("Create Account button routes to the Create Account Page:", async () => {
 
         const browser = await puppeteer.launch()
         
@@ -28,12 +28,12 @@ describe('Test routing, security and login:', () => {
 
         await page.click('[name="start"]');
         await page.waitForTimeout(10);
-        await page.click('[name="registerbutton"]');
+        await page.click('[name="createButton"]');
         await expect(page.title()).resolves.toMatch('OMW! Create account');
         await page.waitForTimeout(1000);
         await browser.close();
      })
-     it('Login directs to HomePage:', async () => {
+     it('Successful Login routes to HomePage:', async () => {
 
         const browser = await puppeteer.launch()
         const page = await browser.newPage()
@@ -43,7 +43,7 @@ describe('Test routing, security and login:', () => {
         await page.click('[name="start"]');
 
         await page.waitForSelector('input[name="username"]');
-        await page.$eval('input[name="username"]', el => el.value = '123');
+        await page.$eval('input[name="username"]', el => el.value = 'Trist');
 
         await page.waitForSelector('input[name="password"]');
         await page.$eval('input[name="password"]', el => el.value = '123');
@@ -54,7 +54,7 @@ describe('Test routing, security and login:', () => {
         await page.waitForTimeout(1000);
         await browser.close();
     })
-    it('Login page reloads when password is incorrect:', async () => {
+    it('Unsuccessful Login due to wrong password reloads the Login Page:', async () => {
 
         const browser = await puppeteer.launch()
         const page = await browser.newPage()
@@ -64,7 +64,7 @@ describe('Test routing, security and login:', () => {
         await page.click('[name="start"]');
 
         await page.waitForSelector('input[name="username"]');
-        await page.$eval('input[name="username"]', el => el.value = '123');
+        await page.$eval('input[name="username"]', el => el.value = 'Trist');
 
         await page.waitForSelector('input[name="password"]');
         await page.$eval('input[name="password"]', el => el.value = '1234');
@@ -75,19 +75,17 @@ describe('Test routing, security and login:', () => {
         await page.waitForTimeout(1000);
         await browser.close();
     })
-     it('Login button says login', async () => {
-
+    it("Attempting to go to menu without login directs you to login page:", async () => {
+        
         const browser = await puppeteer.launch()
         const page = await browser.newPage()
-        await page.setViewport({width: 1920, height: 1080});
-        await page.goto('https://ohmywordle.azurewebsites.net/')
+        await page.goto('https://ohmywordle.azurewebsites.net/home')
 
-        await page.click('[name="landing"]');
-        const text = await page.$eval('button', element => element.textContent)
+        //await page.click('[name="start"]');
         await page.waitForTimeout(10);
-        await expect(text).toMatch('Login');
+        await expect(page.title()).resolves.toMatch('OMW! Login');
         await page.waitForTimeout(1000);
         await browser.close();
-    })  
-    */
+     })
+
 })
