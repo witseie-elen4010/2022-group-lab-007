fc143c1f51ff19116d7c7a7c084a3cb55957450b
TristandeGroot <tristankyledegroot@gmail.com>
2022-06-05 19:03:20 +0200

    Login Testing

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -1,6 +1,6 @@
 /* eslint-env jest*/
 const puppeteer = require('puppeteer');
-const timeout = 60000;
+const timeout = 90000;
 jest.setTimeout(timeout);
 
 describe('Test index page:', () => {
@@ -11,9 +11,83 @@ describe('Test index page:', () => {
         const page = await browser.newPage()
         await page.goto('https://ohmywordle.azurewebsites.net/')
 
-        await page.click('[name="landing"]');
-
-        await expect(page.title()).resolves.toMatch('OMW! Login');
+        await page.click('[name="start"]');
+        await page.waitForTimeout(10);
+        await expect(page.title()).resolves.toMatch('Login Page');
+        await page.waitForTimeout(1000);
         await browser.close();
      })
+     /*
+     it("Register button redirects to Create Account Page:", async () => {
+
+        const browser = await puppeteer.launch()
+        
+        const page = await browser.newPage()
+        await page.setViewport({width: 1920, height: 1080});
+        await page.goto('https://ohmywordle.azurewebsites.net/')
+
+        await page.click('[name="start"]');
+        await page.waitForTimeout(10);
+        await page.click('[name="registerbutton"]');
+        await expect(page.title()).resolves.toMatch('Create account');
+        await page.waitForTimeout(1000);
+        await browser.close();
+     })
+     it('Login directs to HomePage:', async () => {
+
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+        await page.setViewport({width: 1920, height: 1080});
+        await page.goto('https://ohmywordle.azurewebsites.net/')
+        
+        await page.click('[name="landing"]');
+
+        await page.waitForSelector('input[name="username"]');
+        await page.$eval('input[name="username"]', el => el.value = '123');
+
+        await page.waitForSelector('input[name="password"]');
+        await page.$eval('input[name="password"]', el => el.value = '123');
+
+        await page.click('[name="loginbutton"]');
+        await page.waitForTimeout(10);
+        await expect(page.title()).resolves.toMatch('Homepage');
+        await page.waitForTimeout(1000);
+        await browser.close();
+    })
+    it('Login page reloads when password is incorrect:', async () => {
+
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+        await page.setViewport({width: 1920, height: 1080});
+        await page.goto('https://ohmywordle.azurewebsites.net/')
+        
+        await page.click('[name="landing"]');
+
+        await page.waitForSelector('input[name="username"]');
+        await page.$eval('input[name="username"]', el => el.value = '123');
+
+        await page.waitForSelector('input[name="password"]');
+        await page.$eval('input[name="password"]', el => el.value = '1234');
+
+        await page.click('[name="loginbutton"]');
+        await page.waitForTimeout(10);
+        await expect(page.title()).resolves.toMatch('Login Page');
+        await page.waitForTimeout(1000);
+        await browser.close();
+    })
+     it('Login button says login', async () => {
+
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+        await page.setViewport({width: 1920, height: 1080});
+        await page.goto('https://ohmywordle.azurewebsites.net/')
+
+        await page.click('[name="landing"]');
+        const text = await page.$eval('button', element => element.textContent)
+        await page.waitForTimeout(10);
+        await expect(text).toMatch('Login');
+        await page.waitForTimeout(1000);
+        await browser.close();
+    })  
+    */
 })
