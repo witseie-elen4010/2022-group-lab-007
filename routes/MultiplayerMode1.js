aed19ca99fbadce59bc46b53d2e765e2572871ae
TristandeGroot <tristankyledegroot@gmail.com>
2022-06-06 22:04:27 +0200

    scoreboard update from multiwordle communication established

--- routes/MultiplayerMode1.js
+++ routes/MultiplayerMode1.js
@@ -12,8 +12,11 @@ router.get('/', (req, res) => {
 
 // after multiwordle game finishes
 router.post('/', (req, res) => {
-    const gameWon = req.body.gameWon
-    const userID = req.session.id
+    console.log("I made it!!!")
+    //const gameWon = req.body.gameWin
+    const gameWon = true
+    //const userID = req.session.id
+    let userID = 32
     let score
 
     //Aquire games won and games played from score table
@@ -21,13 +24,16 @@ router.post('/', (req, res) => {
     .then((pool)=>{
         return pool.request()
         .input('userID',userID)
-        .query('SELECT (games,wins) FROM dbo.scoreboard WHERE userID = @userID')
+        .query('SELECT games,wins FROM dbo.scoreboard WHERE userID = @userID')
     }).then(result=>{
+        console.log("I made it here 2!!!")
         let games = result.recordset[0].games
+        console.log("I made it here 3!!!")
         let wins = result.recordset[0].wins
         // update values and calculate new score
         if(gameWon===true){
             games = games + 1
+            console.log("I made it here 4!!!")
             wins = wins + 1
             score = wins/games
         }else{
@@ -41,8 +47,9 @@ router.post('/', (req, res) => {
             .input('games',games)
             .input('wins',wins)
             .input('score',score)
-            .query('INSERT INTO dbo.scoreboard (games,wins,score) VALUES (@games,@wins,@score) WHERE userID = @userID')
+            .query('INSERT INTO dbo.scoreboard (games,wins,score) VALUES (@games,@wins,@score) WHERE userID = @userID;')
         }).then(result=>{
+            console.log("I made it 5!!!")
             // route to home page
             res.redirect('/home')
         })
